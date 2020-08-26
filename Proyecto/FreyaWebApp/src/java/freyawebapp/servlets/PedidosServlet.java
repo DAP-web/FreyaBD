6ac421779fc1a6485a22b1bd9b71282a6db288cb
Ángela Ruiz <ac.comayagua@yahoo.com>
2020-08-26 01:38:11 -0600

    Pedidos
    jiji

--- Proyecto/FreyaWebApp/src/java/freyawebapp/servlets/PedidosServlet.java
+++ Proyecto/FreyaWebApp/src/java/freyawebapp/servlets/PedidosServlet.java
@@ -1,88 +1,116 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package freyawebapp.servlets;
 
+import freyawebapp.logic.ClientLogic;
+import freyawebapp.logic.PlatillosLogic;
+import freyawebapp.logic.PedidosLogic;
+import freyawebapp.objects.ClientObject;
+import freyawebapp.objects.PlatilloObject;
+import freyawebapp.objects.PedidosObject;
+import freyawebapp.objects.PedidosViewObject;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author accom
- */
 @WebServlet(name = "PedidosServlet", urlPatterns = {"/PedidosServlet"})
 public class PedidosServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet PedidosServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet PedidosServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String strConnString = "jdbc:mysql://localhost/freya1?"
+                + "user=root&password=12345&"
+                + "autoReconnect=true&useSSL=false";
+        String strformid = request.getParameter("formid");
+        PedidosLogic logic;
+        request.getSession().setAttribute("rows", 0);
+        
+        String strIdpedidos, strIdcliente, strIdplatillos;
+        int iIdpedidos, iIdCliente, iIdPlato, rows;
+        
+        ClientLogic clientLogic;
+        PlatillosLogic platillosLogic;
+                
+        ArrayList<ClientObject> clientArray;
+        ArrayList<PlatilloObject> platilloArray;
+        
+        switch(strformid){
+            case "1":
+                System.out.println("Código para ingresar un nuevo pedido...");
+                
+                //al inicio
+                strIdcliente = request.getParameter("lastname");
+                iIdCliente = Integer.parseInt(strIdcliente);
+                strIdplatillos = request.getParameter("numeroplato");
+                iIdPlato = Integer.parseInt(strIdplatillos);
+                
+                //enmedio
+                logic = new PedidosLogic(strConnString);
+                rows = logic.insertNewPedido(iIdCliente, iIdPlato);
+                
+                //al final
+                request.getSession().setAttribute("rows", rows);
+                response.sendRedirect("PedidosServlet?formid=3");
+                break;
+            case "2":
+                System.out.println("Código para eliminar un pedido...");
+                
+                //al inicio
+                strIdpedidos = request.getParameter("id");
+                iIdpedidos = Integer.parseInt(strIdpedidos);
+                
+                //enmedio
+                logic = new PedidosLogic(strConnString);
+                rows = logic.deletePedidos(iIdpedidos);
+                
+                //al final
+                request.getSession().setAttribute("rows", rows);
+                response.sendRedirect("PedidosServlet?formid=3");
+                break;
+            case "3":
+                System.out.println("Traer todas los pedidos...");
+                //enmedio
+                logic = new PedidosLogic(strConnString);
+                ArrayList<PedidosViewObject> pedidosArray = logic.getAllPedidos();
+                
+                //al final
+                request.getSession().setAttribute("pedidosArray", pedidosArray);
+                response.sendRedirect("pedidos.jsp");
+                break;
+            case "4":
+                /*
+                System.out.println("Get parts for update 1...");
+                
+                strIdreserva = request.getParameter("id");
+                iIdreserva = Integer.parseInt(strIdreserva);
+                
+                logic = new ReservaLogic(strConnString);
+                ReservaObject reservaobj = logic.getReservaByID(iIdreserva);
+                
+                request.getSession().setAttribute("reservaobj", reservaobj);
+                response.sendRedirect("newReserva.jsp");
+                */
+                break;
+            case "5":
+                break;
+            case "6":
+                System.out.println("Get all parts for new pedido");
+                
+                clientLogic = new ClientLogic (strConnString);
+                platillosLogic = new PlatillosLogic (strConnString);
+                
+                clientArray = clientLogic.getAllClients();
+                platilloArray = platillosLogic.getAllPlatillos();
+                
+                request.getSession().setAttribute("clientArray", clientArray);
+                request.getSession().setAttribute("platilloArray", platilloArray);
+                response.sendRedirect("newPedido.jsp");
+                break;
+            default:
+                break;
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    
 }
--- Proyecto/FreyaWebApp/web/PedidosMain.jsp
+++ Proyecto/FreyaWebApp/web/PedidosMain.jsp
@@ -4,14 +4,84 @@
     Author     : accom
 --%>
 
+<%@page import="java.util.Iterator"%>
+<%@page import="freyawebapp.objects.PedidosViewObject"%>
+<%@page import="java.util.ArrayList"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Pedidos</title>
+        <style>
+            h1{
+                color: #000000;
+                text-align:center;
+                font-size: 40px;
+            }
+            p{
+                color: #000000;
+                font-size: 20px;
+            }
+            h3{
+                color: #000080;
+                text-align:center;
+                font-size: 20px;
+            }
+        </style>
     </head>
-    <body>
-        <h1>Hello World!</h1>
+     <%
+            int rows = (int)request.getSession().getAttribute("rows");
+            ArrayList<PedidosViewObject> array = (ArrayList<PedidosViewObject>)request.getSession().getAttribute("PedidosArray");
+        %>
+    <body bgcolor="#FFDAB9" >
+        
+        <h1>Pedidos</h1>
+         <h3>Cliente y platillo</h3>
+        <p>Aquí irá una view que permita ver y modificar los pedios de los cliente</p>
+        <br><br>
+        <a href="PedidosServlet?formid=6" style="font-size: 1.25em">Nuevo Pedido</a>
+        <br><br>
+        <%
+        if(rows>0)
+        {
+        %>
+            <p style="color:red;"><%= rows %> affected</p>
+            <br><br>
+        <%
+        }
+        %>
+          <table style="width:40%" border="1">
+          <tr>
+            <th>Client Last Name</th>
+            <th>Client Name</th>
+            <th>Plato</th>
+            <th <!--colspan="2"-->Acciones</th>
+          </tr>
+                    <%
+              Iterator<PedidosViewObject> iteArray= null;
+              if(array!=null)
+              {
+                  iteArray = array.iterator();
+                  PedidosViewObject temp = null;
+                  while(iteArray.hasNext())
+                  {
+                      temp = iteArray.next();
+          %>
+               <tr>
+               <td><%= temp.getLastName() %></td>
+               <td><%= temp.getName() %></td>
+               <td style="text-align:center"><%= temp.getNombrePlato() %></td><!--
+               <td><a href="SectionServlet?formid=4&id=<%= temp.getPedidosID() %>">Update</a></td>-->
+               <td><a href="PedidosServlet?formid=2&id=<%= temp.getPedidosID() %>">Delete</a></td>
+               </tr> 
+          <%
+                   }
+              }
+          %>
+                    
+        </table>
+        <br><br>
+        <a href="RestauranteServlet?formid=3" style="font-size: 1.25em">Volver</a>
     </body>
 </html>
